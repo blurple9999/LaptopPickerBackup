4e333c6b82a62515685fa2ecac57ae638a867d7f
blurple9999 <100024026+blurple9999@users.noreply.github.com>
2022-11-27 15:07:43 -0500

    fix + checks, see line 21
    //the following set of if statements check if an input is empty. input
    //can be empty for 2 reasons: user first enters the site, or user
    //deselects all options within a category. if empty, set the specified
    //filter to show all within that category; this is to prevent errors

--- SQL/filter.php
+++ SQL/filter.php
@@ -9,7 +9,7 @@ function create_task_table1() {
         echo "<td>" . $r[0] . "</td>\n";
         echo "<td>" . $r[1] . "</td>\n";
         echo "<td>" . $r[2] . "</td>\n";
-        //echo "<td>" . $r[3] . "</td>\n";
+        //echo "<td>" . $r[3] . "</td>\n"; //deprecated
         echo "</tr>\n";
     }
 }
@@ -18,36 +18,79 @@ function db_get_task_data1() {
     require("credentials.php");
     $conn = mysqli_connect($servername,$username,$password,$database);
     
+    //the following set of if statements check if an input is empty. input
+    //can be empty for 2 reasons: user first enters the site, or user
+    //deselects all options within a category. if empty, set the specified
+    //filter to show all within that category; this is to prevent errors
+    
     //price filter stuff
-    $prices = $_POST["price_range"];
-	$price_arr = explode("-", "$prices");
+    $defaultPriceMin = 0;
+    $defaultPriceMax = 5000;
+    //$prices = $_POST["price_range"]; //deprecated
+	if (!empty($_POST["price_range"])){
+	    $price_arr = explode("-", $_POST["price_range"]);
+	} else {
+	    $price_arr[0] = $defaultPriceMin;
+	    $price_arr[1] = $defaultPriceMax;
+	}
 	
 	//cpu filter stuff
-	$cpuseries = '(\'' . implode('\',\'', $_POST["CPUInput"]) .'\')';
-	
+	if (!empty($_POST["CPUInput"])){
+	    $cpuseries = '(\'' . implode('\',\'', $_POST["CPUInput"]) .'\')';
+	} else { $cpuseries = '(\'AMD Ryzen 3\',\'AMD Ryzen 5\',\'AMD Ryzen 7\',\'AMD Ryzen 9\',
+	           \'Intel Core i3\',\'Intel Core i5\',\'Intel Core i7\',\'Intel Core i9\')'; }
+	            
 	//storage filter stuff
-    $storage_arr = '(\'' . implode('\',\'', $_POST["storageInput"]) .'\')';
+	if (!empty($_POST["storageInput"])){
+        $storage_arr = '(\'' . implode('\',\'', $_POST["storageInput"]) .'\')';
+	} else { $storage_arr = '(\'128\',\'256\',\'512\',\'1024\',\'2048\')'; }
 	
 	//ram filter stuff
-	$ram_arr = '(\'' . implode('\',\'', $_POST["ramInput"]) .'\')';
+	if (!empty($_POST["ramInput"])){
+	    $ram_arr = '(\'' . implode('\',\'', $_POST["ramInput"]) .'\')';
+	} else { $ram_arr = '(\'4\',\'8\',\'16\',\'32\')'; }
 	
 	//OS filter stuff
-	$OS_arr = '(\'' . implode('\',\'', $_POST["osInput"]) .'\')';
+	if (!empty($_POST["osInput"])){
+	    $OS_arr = '(\'' . implode('\',\'', $_POST["osInput"]) .'\')';
+	} else { $OS_arr = '(\'Windows 10\',\'Windows 11\')'; }
 	
 	//refresh rate filter
-	$rr_arr = '(\'' . implode('\',\'', $_POST["refreshInput"]) .'\')';
+	if (!empty($_POST["refreshInput"])){
+	    $rr_arr = '(\'' . implode('\',\'', $_POST["refreshInput"]) .'\')';
+	} else { $rr_arr = '(\'60\',\'90\',\'120\',\'144\')'; }
 	
 	//aspect ratio filter
-	$ratio_arr = '(\'' . implode('\',\'', $_POST["ratioInput"]) .'\')';
+	if (!empty($_POST["ratioInput"])){
+	    $ratio_arr = '(\'' . implode('\',\'', $_POST["ratioInput"]) .'\')';
+	} else { $ratio_arr = '(\'3:2\',\'16:9\',\'16:10\')'; }
 	
 	//manufacturer filter
-	$manufacturer_arr = '(\'' . implode('\',\'', $_POST["manufactureInput"]) .'\')';
+	if (!empty($_POST["manufactureInput"])){
+	    $manufacturer_arr = '(\'' . implode('\',\'', $_POST["manufactureInput"]) .'\')';
+	} else { $manufacturer_arr = '(\'Acer\',\'Samsung\',\'Lenovo\',\'Dell\',\'HP\',\'Microsoft\',\'Asus\')'; }
 	
 	//touchscreen filter
-	$touch_arr = '(\'' . implode('\',\'', $_POST["touchInput"]) .'\')';
+	if (!empty($_POST["touchInput"])){
+	    $touch_arr = '(\'' . implode('\',\'', $_POST["touchInput"]) .'\')';
+	} else { $touch_arr = '(\'Yes\',\'No\')'; }
 	
 	//rj45 filter
-	$rj45_arr = '(\'' . implode('\',\'', $_POST["rj45Input"]) .'\')';
+	if (!empty($_POST["rj45Input"])){
+	    $rj45_arr = '(\'' . implode('\',\'', $_POST["rj45Input"]) .'\')';
+	} else { $rj45_arr = '(\'Yes\',\'No\')'; }
+	
+	/* debugging purposes
+	echo $cpuseries;
+	echo $storage_arr;
+	echo $ram_arr;
+	echo $OS_arr;
+	echo $rr_arr;
+	echo $ratio_arr;
+	echo $manufacturer_arr;
+	echo $touch_arr;
+	echo $rj45_arr;
+	*/
 	
 	//grab list
     $query = "SELECT Laptop, `CPU Series`, CPU, Price FROM laptop_list
@@ -62,6 +105,7 @@ function db_get_task_data1() {
     AND Touchscreen IN $touch_arr
     AND RJ45 IN $rj45_arr
     ;";
+    //echo $query; //debugging purposes
     
     //push result to create_task_table()
     $result = mysqli_query($conn,$query);
