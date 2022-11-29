3438dc5fe13166ce71f2066dc90f5db41e504525
blurple9999 <100024026+blurple9999@users.noreply.github.com>
2022-11-28 21:32:35 -0500

    assignment 5

---
+++ testClasstest.php
@@ -0,0 +1,58 @@
+<?php
+final class testClass{
+	private $price_arr;
+	private $storage_arr;
+	private $ram_arr;
+	
+	private function __construct() {}	
+	public function createPrice($post_price){
+		if (!empty($post_price)){
+			$price_arr = explode("-", $post_price);
+		} else {
+			$price_arr[0] = 0;
+			$price_arr[1] = 5000;
+		}
+		return $price_arr;
+	}
+	
+	public function createStorage($post_storage){
+		if (!empty($post_storage)){
+			$storage_arr = '(\'' . implode('\',\'', $post_storage) .'\')';
+		} else { $storage_arr = '(\'128\',\'256\',\'512\',\'1024\',\'2048\')'; }
+		return $storage_arr;
+	}
+	
+	public function createRam($post_ram){
+		if (!empty($post_ram)){
+			$ram_arr = '(\'' . implode('\',\'', $post_ram) .'\')';
+		} else { $ram_arr = '(\'4\',\'8\',\'16\',\'32\')'; }
+		return $ram_arr;
+	}
+	
+	//testing
+	//public function returnPrice(){ return $price_arr; }
+	//public function returnStorage(){ return $storage_arr; }
+	//public function returnRam(){ return $ram_arr; }
+}
+use PHPUnit\Framework\TestCase;
+final class testClasstest extends TestCase{
+	public function testPrice1(){
+		$this->assertEquals([10, 20], testClass::createPrice('10-20'));
+	}
+	public function testPrice2(){
+		$this->assertEquals([0, 5000], testClass::createPrice(NULL));
+	}
+	public function testStorage1(){
+		$this->assertEquals('(\'128\',\'256\')', testClass::createStorage([128,256]));
+	}
+	public function testStorage2(){
+		$this->assertEquals('(\'128\',\'256\',\'512\',\'1024\',\'2048\')', testClass::createStorage(NULL));
+	}
+	public function testRam1(){
+		$this->assertEquals('(\'8\',\'16\')', testClass::createRam([8,16]));
+	}
+	public function testRam2(){
+		$this->assertEquals('(\'4\',\'8\',\'16\',\'32\')', testClass::createRam(NULL));
+	}
+}
+?>
