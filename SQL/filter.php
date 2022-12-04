1e55548443f833dca6fa9505a961ea9456610e7c
blurple9999 <100024026+blurple9999@users.noreply.github.com>
2022-12-04 17:54:07 -0500

    added link to query, output

--- SQL/filter.php
+++ SQL/filter.php
@@ -6,10 +6,9 @@ function create_task_table1() {
 	// Create table with task data
 	foreach ($tasks as $r) {
 		echo "<tr>\n";
-		echo "<td>" . $r[0] . "</td>\n";
+		echo "<td><a href=\"" . $r[3] . "\">" . $r[0] . "</a></td>\n";
 		echo "<td>" . $r[1] . "</td>\n";
 		echo "<td>" . $r[2] . "</td>\n";
-		//echo "<td>" . $r[3] . "</td>\n"; //deprecated
 		echo "</tr>\n";
 	}
 }
@@ -109,7 +108,7 @@ function db_get_task_data1() {
 	*/
 	
 	//grab list
-	$query = "SELECT Laptop, `CPU Series`, CPU, Price FROM laptop_list
+	$query = "SELECT Laptop, `CPU Series`, CPU, Price, Link FROM laptop_list
 	WHERE Price BETWEEN $price_arr[0] AND $price_arr[1]
 	$cpuseries
 	$storage_arr
@@ -129,7 +128,7 @@ function db_get_task_data1() {
 	while ($row = mysqli_fetch_row($result)) {
 		$temp = array();
 		$cpu = "$row[1] $row[2]";
-		array_push($temp,$row[0],$cpu,$row[3]);
+		array_push($temp,$row[0],$cpu,$row[3],$row[4]);
 		array_push($relation,$temp);
 	}
 	mysqli_close($conn);
