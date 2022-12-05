f32229cb9f5225c3b1cdd43096eb8b3893e5243b
mehedizihad <mehedi.zihad@wayne.edu>
2022-12-05 11:28:29 -0500

    Fix issues with Search Bar

--- HTML/search.php
+++ HTML/search.php
@@ -1,55 +1,108 @@
-<!DOCTYPE html>
-<html>
+<?php
+	include("filter.php");
+?>
+
+<!doctype html>
+<html lang="en">
 <head>
-	<title>Search Bar using PHP</title>
+<meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    
+    <meta name="description" content="Laptop Picker">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+	<link href="asset/images/logo/logo.jpeg" rel="icon">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kristi%7cPoppins:400,500,600,700%7cYeseva+One&amp;display=swap">
+    <link rel="stylesheet" href="asset/css/libraries.css" />
+    <link rel="stylesheet" href="asset/css/style.css" />
+
+
+    <title>Laptop Picker</title>
 </head>
 <body>
 
-<form method="post">
-<label>Search</label>
-<input type="text" name="search">
-<input type="submit" name="submit">
-	
-</form>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card mt-4">
+                    <div class="card-header">
+                        <h4>Laptop Picker</h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-7">
 
+                                <form action="" method="GET">
+                                    <div class="input-group mb-3">
+                                        <input type="text" name="search" required value="<?php if(isset($_GET['search'])){echo $_GET['search']; } ?>" class="form-control" placeholder="Search data">
+                                        <button type="submit" class="btn btn-primary">Search</button>
+                                    </div>
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-12">
+                <div class="card mt-4">
+                    <div class="card-body">
+                        <table class="table table-bordered">
+                            <thead>
+							<tr>
+								<th>Laptop </th>
+								<th>CPU</th>
+								<th>Price</th>
+							</tr>
+                            </thead>
+                            <tbody>
+                                <?php 
+                                    $con = mysqli_connect("localhost","root","","laptop_list");
+
+                                    if(isset($_GET['search']))
+                                    {
+                                        $filtervalues = $_GET['search'];
+                                        $query = "SELECT * FROM laptop_list WHERE CONCAT(laptop,CPU, price) LIKE '%$filtervalues%' ";
+                                        $query_run = mysqli_query($con, $query);
+
+                                        if(mysqli_num_rows($query_run) > 0)
+                                        {
+                                            foreach($query_run as $items)
+                                            {
+                                                ?>
+                                                <tr>
+                                                    <td><?= $items['Laptop']; ?></td>
+                                                    <td><?= $items['CPU']; ?></td>
+                                                    <td><?= $items['Price']; ?></td>
+                                                    
+                                                </tr>
+                                                <?php
+                                            }
+                                        }
+                                        else
+                                        {
+                                            ?>
+                                                <tr>
+                                                    <td colspan="4">Your Product not Found</td>
+                                                </tr>
+                                            <?php
+                                        }
+                                    }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
-
-<?php
-
-$con = new PDO("mysql:host=localhost;dbname=laptop_list",'root','');
-
-if (isset($_POST["submit"])) {
-	$str = $_POST["search"];
-	$sth = $con->prepare("SELECT * FROM `search` WHERE Name = '$str'");
-
-	$sth->setFetchMode(PDO:: FETCH_OBJ);
-	$sth -> execute();
-
-	if($row = $sth->fetch())
-	{
-		?>
-		<br><br><br>
-		<table>
-			<tr>
-				<th>Name</th>
-				<th>Description</th>
-			</tr>
-			<tr>
-				<td><?php echo $row->Name; ?></td>
-				<td><?php echo $row->Description;?></td>
-			</tr>
-
-		</table>
-<?php 
-	}
-		
-		
-		else{
-			echo "Name Does not exist";
-		}
-
-
-}
-
-?>
