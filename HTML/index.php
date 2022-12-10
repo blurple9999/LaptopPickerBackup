69fda0dfa5af605e9b57c418e61152ac5058c02a
sanjida-sultana <hg4167@wayne.edu>
2022-12-10 12:33:09 -0500

    make it credentials.php

--- HTML/index.php
+++ HTML/index.php
@@ -4,7 +4,7 @@ session_start();
 
 if (isset($_SESSION["user_id"])) {
     
-    $mysqli = require __DIR__ . "/database.php";
+    $mysqli = require __DIR__ . "/credendials.php";
     
     $sql = "SELECT * FROM user
             WHERE id = {$_SESSION["user_id"]}";
