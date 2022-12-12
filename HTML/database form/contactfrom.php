426c8d8dde9d2382411017ad41c0455c24e7e0c0
KANTA-00 <hj2729@wayne.edu>
2022-12-11 21:35:27 -0500

    update file

---
+++ HTML/database form/contactfrom.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="contact-form">
+        <h1>Contact us</h1>
+    </div>
+    <div class="contact-us">
+       <form action="ThankYou.html" method="post">
+           <input type="text"required id="fname"   class="form-control" placeholder="Enter First Name"> <br>
+           <input type="text" required id="lname"  class="form-control" placeholder="Enter last Name"> <br>
+           <input type="email" required id="email"  class="form-control" placeholder="Enter Email"> <br>
+           <input type="text" required id="mobile"  class="form-control" placeholder="Enter Mobile Number"> <br>
+           <input type="submit" class="form-control submit" value="submit" >
+       </form>
+    </div>
+</body>
+</html>
