9f068a59ed8d0caaf02f82cb54eea091d2b26bb8
mehedizihad <mehedi.zihad@wayne.edu>
2022-12-12 19:24:29 -0500

    fix bugs in contact page

--- HTML/save.php
+++ HTML/save.php
@@ -1,5 +1,6 @@
 <?php
 require("credentials.php");
+$con = mysqli_connect($servername,$username,$password,$database);
 
 $fullname = $_POST["fname"];
 $email = $_POST["email"];
@@ -8,12 +9,6 @@ $comment = $_POST["comment"];
 
 
 
-$mysqli = require __DIR__ . "/credentials.php";
-$mysqli=new mysqli($servername,$username,$password,$database);
-
-$stmt = $mysqli->stmt_init();
-
-$conn = mysqli_connect("localhost", "root", "", "login_db") or die("connection failed");
 $sql = "INSERT INTO contact(Full_name,  Email, Mobile, Comment) VALUES ('{$fullname}','{$email}','{$mobile}','{$comment}' )";
 $result = mysqli_query($conn, $sql) or die("Query Failed!");
 header("location: ThankYou.html");
