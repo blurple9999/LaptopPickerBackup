2467da303848e6a2632ef03af2b977357c6fa360
KANTA-00 <97293791+KANTA-00@users.noreply.github.com>
2022-12-12 19:05:16 -0500

    Add contact table

---
+++ SQL/contact.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 13, 2022 at 12:51 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `login_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `Full_name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `mobile` varchar(50) NOT NULL,
+  `Comment` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`Full_name`, `email`, `mobile`, `Comment`) VALUES
+('sanjida', 'sanjida.shanta64@gmail.com', '3133981648', ''),
+('sanjida sultana', 'sanjida.shanta64@gmail.com', '3133981648', 'this is sanjida'),
+('sanjida sultana', 'sanjida.shanta64@gmail.com', '3133981648', 'this is sanjida nila haque'),
+('sanjida sultana', 'sanjida.shanta64@gmail.com', '3133981648', 'this ti kdjffdjjfd'),
+('sanjida sultana', 'sanjida.shanta64@gmail.com', '3133981648', 'could u please call me'),
+('sanjida sultana', 'sanjida.shanta64@gmail.com', '3133981648', 'thisffgjjhhhhhjuyyttrt'),
+('Laptoppicker', 'sanjida6497@gmail.com', '3133981648', 'this is sanjida'),
+('Laptoppicker', 'sanjida6497@gmail.com', '3133981648', 'this is sanjida'),
+('sanjida sultana', 'sanjida.shanta64@gmail.com', '3133981648', 'this is sanjida'),
+('sanjida sultana', 'sanjida.shanta64@gmail.com', '3133981648', 'thisffgjjhhhhhjuyyttrt'),
+('sanjida sultana', 'sanjida.shanta64@gmail.com', '3133981648', 'thisffgjjhhhhhjuyyttrt'),
+('sanjida sultana', 'sanjida.shanta64@gmail.com', '3133981648', 'rrrrrrrrrrrrrrrr'),
+('sanjida sultana', 'sanjida.shanta64@gmail.com', '3133981648', 'sanjida'),
+('sanjida sultana', 'sanjida.shanta64@gmail.com', '3133981648', 'ddddddddddddddddd'),
+('', '', '', ''),
+('', '', '', ''),
+('', '', '', ''),
+('sanjida sultana', 'sanjida.shanta64@gmail.com', 'ddddddd', 'ssssssssssssssdddsqhjcbsnbc'),
+('Kanta Islam', 'Kanta98syl@gmail.com', '33190909090089', 'bdfgdashgjhsdg');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
